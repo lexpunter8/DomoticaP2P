c54ec1506efacdadf867791ef305332e0f5499a8
Lex Punter <lex.punter@gmail.com>
2015-12-18 09:55:14 +0100

    versie 0.1

--- ArduinoServer/ArduinoServer.ino
+++ ArduinoServer/ArduinoServer.ino
@@ -1,9 +1,18 @@
+//schakelaar aan      uit
+// 1         12054575 12054574
+// 2         12054573 12054572
+// 3         12054571 12054570
+// all       12054562 12054561   
+
+//
 #include <SPI.h>
 #include <Ethernet.h>             // Ethernet library
-#include <RCSwitch.h> 
+#include <RCSwitch.h>             // RCSwitch library
 
-#define infoPin   9
-#define analogPin A0
+#define infoPin   9     // infoPin at pin 9
+#define analogPin A0    
+
+RCSwitch mySwitch = RCSwitch();
 
 //Set Ethernet Shield MAC address  (check yours)
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };   // Ethernet adapter shield S. Oosterhaven
@@ -12,6 +21,9 @@ int ethPort = 3300;                                  // Take a free port (check 
 
 EthernetServer server(ethPort);              // EthernetServer instance (listening on port <ethPort>).
 
+   int maxx = 150;
+   int minn = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(infoPin, OUTPUT);
@@ -19,7 +31,7 @@ void setup() {
   
   Serial.begin(9600);
   Serial.println("started");
-  Serial.println(Ethernet.begin(mac));
+  Serial.println(Ethernet.begin(mac));  
 
   if(Ethernet.begin(mac) == 0){
     Serial.println("Could not obtain IP");
@@ -33,7 +45,21 @@ void setup() {
   server.begin();
 
   Serial.print("IP; ");
-  Serial.print(Ethernet.localIP());
+  Serial.println(Ethernet.localIP());
+
+    // Transmitter is connected to Arduino Pin #10  
+  mySwitch.enableTransmit(6);
+
+  // Optional set pulse length.
+   mySwitch.setPulseLength(320);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+    mySwitch.setProtocol(1);
+  
+  // Optional set number of transmission repetitions.
+   mySwitch.setRepeatTransmit(15);
+
+
 }
 
 void loop() {
@@ -44,4 +70,71 @@ void loop() {
   while(ethernetClient.connected()){
     digitalWrite(infoPin, HIGH);
   }
+
+  
+  setSensor('l', minn, maxx, 1);
+  
 }
+
+void switches(String x, int switchN)
+{
+  
+  if(switchN == 1){
+    if(x == "uit"){
+    mySwitch.send(12054574, 24);
+  }
+  if(x == "aan"){
+    mySwitch.send(12054575, 24);
+  }
+  }
+  if(switchN == 2){
+    if(x == "uit"){
+    mySwitch.send(12054572, 24);
+  }
+  if(x == "aan"){
+    mySwitch.send(12054573, 24);
+  }
+  }
+  if(switchN == 3){
+    if(x == "uit"){
+    mySwitch.send(12054570, 24);
+  }
+  if(x == "aan"){
+    mySwitch.send(12054571, 24);
+  }
+  }
+}
+
+
+void setSensor(char sensor, int minValue, int maxValue, int swn){
+  int lichtValue = analogRead(analogPin);
+  if(sensor == 'l')
+  {
+    if(lichtValue < maxValue && lichtValue > minValue)
+    {  
+      switches("aan", swn);
+    } 
+    else 
+    {
+      switches("uit", swn);
+    }
+  }
+  if(sensor == 't')
+  {
+    if(lichtValue < maxValue && lichtValue > minValue)
+    {  
+      switches("aan", swn);
+    } 
+    else 
+    {
+      switches("uit", swn);
+    }
+  }
+}
+
+void onOff(int s){
+  if(s == 1){
+    
+  }
+}
+
