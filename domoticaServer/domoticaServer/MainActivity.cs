a63d8deb200e189c3ebd951df48f48a996f538eb
Lex Punter <lex.punter@gmail.com>
2015-12-18 10:03:22 +0100

    versie 0.1

---
+++ domoticaServer/domoticaServer/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace domoticaServer
+{
+	[Activity (Label = "domoticaServer", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			System.Console.WriteLine ("hello");
+			// Get our button from the layout resource,
+			// and attach an event to it
+			string conn = domoticaServer.connect_to_server.connect ();
+			//EditText text1 = FindViewById<EditText> (Resource.Id.textView);
+			
+			System.Console.WriteLine (conn);
+
+
+		}
+	}
+}
+
+
+
---
+++ domoticaServer/domoticaServer/connect to server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace domoticaServer
+{
+	public class connect_to_server
+	{
+		public static string connect()
+		{
+			Socket s = open("192.168.0.108", 3300);
+			write (s, "hallo wereld");
+			string reply = read (s);
+			close (s);
+			System.Console.WriteLine (reply);
+			return reply;
+		}
+
+		public static Socket open(string ipaddress, int portnr)
+		{
+			Socket socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPAddress ip = IPAddress.Parse (ipaddress);
+			IPEndPoint endpoint = new IPEndPoint (ip, portnr);
+			System.Console.WriteLine (endpoint);
+			socket.Connect (endpoint);
+			return socket;
+		}
+
+		public static void write(Socket socket, string text)
+		{
+			socket.Send(Encoding.ASCII.GetBytes(text));
+		}
+		public static string read(Socket socket)
+		{
+			byte[] bytes = new byte[4096];
+			int bytesRec = socket.Receive(bytes);
+			string text = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+			return text;
+		}
+		public static void close(Socket socket)
+		{
+			socket.Close();
+		}
+	}
+}
+
